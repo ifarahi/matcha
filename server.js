04fae70235ac365b0e564ea176d2e51bbb9b6d3e
yassine <yassine@localhost.com>
2019-11-14 17:13:53 +0100

    before merge

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 const router = require('./routes');
-var cors = require('cors');
-
+const cors = require('cors');
+// const io = require('socket.io')( app );
 
  
-app.use(cors());
-
-app.use(express.static('public'));
-
+app.use(cors());// Enables cors ( cross origin resources sharing )
+app.use(express.static('public')); // serves static assets 
 app.use(express.json()); // parse the body wich is contain a json object then pass controll the the router
 app.use(router); // all requests will be hanlled by the router
 
+// what is this and also should we just remove serve static assets?
 app.use((err, req, res, next) => {
     res.status(400).json({
         message: err.message
@@ -19,4 +18,10 @@ app.use((err, req, res, next) => {
 });
 
 port = process.env.PORT || 3000; // if there any port number has been exported in the env will be used if not 3000 is the default
+
+// console.log( app )
+// io.on('connection', function(socket){
+//     console.log('a user connected');
+// });
+
 app.listen(port, () => { console.log(`listening on port ${port} ...`) });
