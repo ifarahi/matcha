8f93368535f502d735325cc1f620aca77a5df781
Ismail Farahi <dr0farahi@gmail.com>
2019-12-13 19:11:57 +0100

    changed email regix and handled exeptions on nodemailer

--- helpers/mail_sender.js
+++ helpers/mail_sender.js
@@ -8,32 +8,38 @@ const nodemailer = require('nodemailer');
 module.exports = {
     completeRegistarion: async (data) => { // this method is responsable to only send complete registration message
 
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: process.env.GMAIL_USER,
-                pass: process.env.GMAIL_PASS
-            }
-        });
+        try {
 
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: '[Matcha]', // sender address
-            to: data.email, // list of receivers
-            subject: '[MATCHA] Confirm your account', // Subject line
-            html: `
-            <h1>Please click on the link to complete your registration</h1>
-            <br >
-            <a href="http://localhost:3001/confirmation/${data.email}/${data.verify_email_token}">
-            Click here
-            </a>
-            ` // html body
-        });
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: process.env.GMAIL_USER,
+                    pass: process.env.GMAIL_PASS
+                }
+            });
 
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: '[Matcha]', // sender address
+                to: data.email, // list of receivers
+                subject: '[MATCHA] Confirm your account', // Subject line
+                html: `
+                <h1>Please click on the link to complete your registration</h1>
+                <br >
+                <a href="http://localhost:3001/confirmation/${data.email}/${data.verify_email_token}">
+                Click here
+                </a>
+                ` // html body
+            });
+            
+        } catch (error) {
+            
+        }
+
+
     },
 
     forgetPassword: async (data) => { // this method is responsable to only send a link to re-initialize account password
